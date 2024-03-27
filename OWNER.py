237bce7015b0a8301d07daf72de0539b6ae510df
Salamqq <163534782+Salamqq@users.noreply.github.com>
2024-03-27 19:29:57 +0300

    Update OWNER.py

--- OWNER.py
+++ OWNER.py
@@ -2,11 +2,10 @@
 
 OWNER = ["S_1_02"] 
 OWNER_NAME = "❲ Developer ❳"
-BOT_TOKEN = "6023089134:AAGeK-QXezCL_vEC-C7l4ANtWNrW3COGtBo"
-DATABASE = "mongodb+srv://techz:bots@cluster0.rwj5ffs.mongodb.net/?retryWrites=true&w=majority" 
-CHANNEL = "https://t.me/vlorantt"
-GROUP = "https://t.me/RonyNH1"
+BOT_TOKEN = "7082671295:AAFtDb7w5yy9bwqtEx1Dj_GeKDtrkmcrL4M"
+DATABASE = "mongodb+srv://sasa:asas@cluster0.9098vne.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0" 
+CHANNEL = "https://t.me/A1DIIU"
+GROUP = "https://t.me/ll0llld"
 VIDEO = "https://t.me/photosures/10"
 PHOTO = "https://telegra.ph/file/8ab98f3b6ee6f2d7caf4b.jpg"
-LOGS = "ll0lll"
- 
+LOGS = "A1DIIU"
